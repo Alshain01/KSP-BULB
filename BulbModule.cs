a1a5d63102f896fdf4e0abc588d0c537619b75e7
Alshain01 <alshain01@suddenlink.net>
2016-08-19 17:13:34 -0500

    Update BulbModule.cs
    Optimization Pass

--- BulbModule.cs
+++ BulbModule.cs
@@ -18,22 +18,26 @@ namespace Bulb
         [UI_FloatRange(maxValue = 1, minValue = 0, scene = UI_Scene.Flight, stepIncrement = 0.05f)]
         protected float blue = 0;
 
+        Light light;
+        Renderer emissive;
+
         public override void OnStart(StartState state)
         {
-            red = part.FindModelComponent<Light>().color.r;
-            green = part.FindModelComponent<Light>().color.g;
-            blue = part.FindModelComponent<Light>().color.b;
+            light = part.FindModelComponent<Light>();
+            emissive = part.FindModelComponent<Renderer>();
+            red = light.color.r;
+            green = light.color.g;
+            blue = light.color.b;
         }
 
         public override void OnUpdate()
         {
             if (HighLogic.LoadedSceneIsFlight)
             {
-                Light lgt = part.FindModelComponent<Light>();
-                if (lgt.color.r != red || lgt.color.g != green || lgt.color.b != blue) 
+                if (light.color.r != red || light.color.g != green || light.color.b != blue) 
                 {
-                    lgt.color = new Color(red, green, blue, 1);
-                    part.FindModelComponent<Renderer>().material.SetColor("_EmissiveColor", lgt.color);
+                    light.color = new Color(red, green, blue, 1);
+                    emissive.material.SetColor("_EmissiveColor", light.color);
                 }
             }
         }
