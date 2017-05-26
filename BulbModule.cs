6238b62fa041caf000cc30cb059498aad0448de9
Alshain01 <alshain01@suddenlink.net>
2017-05-26 10:55:35 -0500

    Update for 1.3

--- BulbModule.cs
+++ BulbModule.cs
@@ -16,10 +16,14 @@ namespace Bulb
         [UI_FloatRange(maxValue = 1, minValue = 0, scene = UI_Scene.Flight, stepIncrement = 0.05f)]
         protected float blue = 0;
 
+        [KSPField(guiActive = false, isPersistant = true)]
+        bool bulbColorRecorded = false;
+
         Light light;
         Renderer emissive;
 
-        public override void OnStart(StartState state)
+
+        public override void OnAwake()
         {
             light = part.FindModelComponent<Light>();
             emissive = part.FindModelComponent<Renderer>();
@@ -28,20 +32,26 @@ namespace Bulb
         public void Update()
         {
             if (HighLogic.LoadedSceneIsFlight)
-                setLightColor();
-            else if (HighLogic.LoadedSceneIsEditor)
-                recordLightColor();
+                if (!bulbColorRecorded)
+                    recordLightColor();
+                else
+                    setLightColor();
+            //else if (HighLogic.LoadedSceneIsEditor)
+                //recordLightColor();
         }
 
         public void recordLightColor()
         {
+            if (light == null) return;
             red = light.color.r;
             green = light.color.g;
             blue = light.color.b;
+            bulbColorRecorded = true;
         }
 
         public void setLightColor()
         {
+            if (emissive == null || light == null) return;
             if (light.color.r != red || light.color.g != green || light.color.b != blue)
             {
                 light.color = new Color(red, green, blue, 1);
